1a78291cfedcd737b9d9fa4c49ba54345dd876c1
chooseanother <kim.tranberg@gmail.com>
2021-05-03 13:55:07 +0200

    deposit adapter

--- src/Deposit.java
+++ src/Deposit.java
@@ -1,3 +1,66 @@
-public class Deposit {
-    private int amount;
+import utility.collection.ArrayList;
+import utility.collection.ListADT;
+
+public class Deposit<T> implements ListADT<T>{
+    private ListADT<T> list;
+
+    public Deposit(){
+        list = new ArrayList<>();
+    }
+
+    @Override
+    public synchronized void add(int index, T element) {
+
+    }
+
+    @Override
+    public synchronized void add(T element) {
+
+    }
+
+    @Override
+    public synchronized void set(int index, T element) {
+
+    }
+
+    @Override
+    public synchronized T get(int index) {
+        return null;
+    }
+
+    @Override
+    public synchronized T remove(int index) {
+        return null;
+    }
+
+    @Override
+    public synchronized T remove(T element) {
+        return null;
+    }
+
+    @Override
+    public synchronized int indexOf(T element) {
+        return 0;
+    }
+
+    @Override
+    public synchronized boolean contains(T element) {
+        return false;
+    }
+
+    @Override
+    public synchronized boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public synchronized boolean isFull() {
+        return false;
+    }
+
+    @Override
+    public synchronized int size() {
+        return 0;
+    }
 }
+
---
+++ src/testDeposit.java
@@ -0,0 +1,9 @@
+import utility.collection.ListADT;
+
+public class testDeposit {
+    public static void main(String[] args) {
+        ListADT<String> deposit = new Deposit<>();
+
+        deposit.add("test");
+    }
+}
