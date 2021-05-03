415c41be245c23c557f01a7dbcb56b9b1f52f3f6
Luka69-420 <lukaudovicic10@gmail.com>
2021-05-03 14:53:41 +0200

    added mine class

---
+++ src/Mine.java
@@ -0,0 +1,14 @@
+public class Mine
+{
+    private int valuable;
+
+    public Mine(int valuable)
+    {
+        this.valuable = valuable;
+    }
+
+    public void getValuable()
+    {
+         valuable = 10;
+    }
+}
