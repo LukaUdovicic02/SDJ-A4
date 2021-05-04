507497fd71f74082b49293092ba380dc3b7c4c2a
hanchry <jurco10510@gmail.com>
2021-05-04 13:45:29 +0200

    ...

--- src/King.java
+++ src/King.java
@@ -1,6 +1,23 @@
 public class King implements Runnable{
+
+    private TreasureRoomDoor<String> treasureRoomDoor;
+
+    public King(TreasureRoomGuardsman<String> treasureRoomGuardsman){
+        treasureRoomDoor = treasureRoomGuardsman;
+    }
+
     @Override
     public void run() {
-
+        while(true){
+            try
+            {
+                Thread.sleep(1000);
+            }
+            catch(InterruptedException e){
+                e.printStackTrace();
+            }
+            //treasureRoomDoor.acquireWrite();
+            //treasureRoomDoor.releasWrite();
+        }
     }
 }
